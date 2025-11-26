db631f74f5af254cc2604593045ed031ebd49932
Hung Randy <lequochung2001@gmail.com>
2025-11-26 16:14:28 +0700

    fix

--- database.php
+++ database.php
@@ -13,7 +13,11 @@ $capsule->addConnection([
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
+    'options'   => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci",
+    ],
 ]);
 
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
+$capsule->getConnection()->statement("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
